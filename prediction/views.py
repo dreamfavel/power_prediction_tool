ca1bc639a358b5f8447e2e426915a1e59352a191
adaniky <paladdick@gmail.com>
2019-06-12 18:53:25 +0300

    Add plot and Bootstrap

--- prediction/views.py
+++ prediction/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 import pickle
-
+from matplotlib import pyplot as plt
 
 from . import feature_selection
 # Create your views here.
@@ -28,13 +28,26 @@ def home(request):
         check_current = feature_selection.date_now(df)
         result['fact'] = [None for i in range(len(result['predict']))]
         result['error'] = [None for i in range(len(result['predict']))]
+        result['predict']=result['predict'].astype(float).round(2)
         if check_current['past']:
             actual = feature_selection.actual_value(check_current)
             if len(actual) == len(result['predict']):
                 result['fact'] = actual
                 result['error'] = 100*abs(result['fact'] - result['predict'])/result['fact']
                 result['error'] = result['error'].round(2)
-                result['predict']=result['predict'].astype(float).round(2)
+
                 tag = '1'
+                result[['fact','predict']].plot(figsize=(10,8), grid=True,style = ['g-','b-'],
+                                                        linewidth=2,
+                                                        title='Результат прогнозування').set_ylabel("МВт")
+        else:
+            result[['predict']].plot(figsize=(10,8), grid=True,style = ['g-'],
+                                                    linewidth=2,
+                                                    title='Результат прогнозування').set_ylabel("МВт")
+            tag = '0'
+        plt.savefig('static/figure.png')
     return render(request, 'home.html',
                     {"result":result, "tag":tag},)
+
+def figure(request):
+    return render(request, 'figure.html')
